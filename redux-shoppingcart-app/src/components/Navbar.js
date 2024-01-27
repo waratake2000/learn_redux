131f3be5ae615a9c3e023a8777db7584d9341f46
waratake2000 <waratake.dev2000@gmail.com>
2024-01-27 09:01:16 +0900

    inital commit

---
+++ redux-shoppingcart-app/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Navbar = () => {
+    return <nav>
+        <div className="nav-center">
+            <h3>Redux Shopping</h3>
+        </div>
+    </nav>
+}
+
+export default Navbar;
+
---
+++ redux-shoppingcart-app/src/features/cart/CartSlice.js
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// 買い物かごの初期化
+const initialState = {
+    cartItems: [],
+    amount: 0,
+    total: 0,
+}
+
+const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {}
+});
+
+export default cartSlice.reducer;
